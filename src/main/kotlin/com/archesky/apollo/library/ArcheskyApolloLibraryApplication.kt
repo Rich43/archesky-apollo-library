b221814491500289bdcc4f03127abcc5b75d4eca
Richie Ward <RichieS@GMail.com>
2020-09-29 19:21:17 +0100

    Initial commit

---
+++ src/main/kotlin/com/archesky/apollo/library/ArcheskyApolloLibraryApplication.kt
@@ -0,0 +1,11 @@
+package com.archesky.apollo.library
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArcheskyApolloLibraryApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArcheskyApolloLibraryApplication>(*args)
+}
---
+++ src/test/kotlin/com/archesky/apollo/library/ArcheskyApolloLibraryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.archesky.apollo.library
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArcheskyApolloLibraryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
