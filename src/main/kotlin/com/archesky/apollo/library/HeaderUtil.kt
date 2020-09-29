fc72fa4cef818fee3b38f43b0bd8fb00c8aa273f
Richie Ward <RichieS@GMail.com>
2020-09-29 19:28:11 +0100

    Add header and service

---
+++ src/main/kotlin/com/archesky/apollo/library/HeaderUtil.kt
@@ -0,0 +1,38 @@
+@file:Suppress("unused")
+
+package com.archesky.common.library
+
+import graphql.kickstart.servlet.context.DefaultGraphQLServletContext
+import graphql.schema.DataFetchingEnvironment
+import java.net.MalformedURLException
+import java.net.URL
+import javax.servlet.http.HttpServletRequest
+
+object HeaderUtil {
+    fun getHost(request: HttpServletRequest): String {
+        try {
+            return request.getHeader("hostname")
+        } catch (e: IllegalStateException) {
+            // Do nothing
+        }
+        try {
+            return request.getHeader("Host")
+        } catch (e: IllegalStateException) {
+            // Do nothing
+        }
+        try {
+            return URL(request.getHeader("Origin")).host
+        } catch (e: IllegalStateException) {
+            // Do nothing
+        } catch (e: MalformedURLException) {
+            // Do nothing
+        }
+        return "localhost"
+    }
+
+    fun getHost(dataFetchingEnvironment: DataFetchingEnvironment): String {
+        val defaultContext = dataFetchingEnvironment.getContext<DefaultGraphQLServletContext>()
+        val request = defaultContext.httpServletRequest;
+        return getHost(request)
+    }
+}
